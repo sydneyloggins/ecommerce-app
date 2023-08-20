b4c2b2aaf2b3f0902f4d235a624ca44dd1d95797
Sydney Loggins <sydneyloggins@Sydneys-Mini.attlocal.net>
2023-08-20 12:24:29 -0500

    added review stars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import {Outlet} from 'react-router-dom'
 import Header from './components/header'
 import Footer from './components/footer'
-import HomeScreen from './screens/HomeScreen'
+
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-    <HomeScreen />
+          <Outlet />
     </Container>
   </main>
   <Footer />
